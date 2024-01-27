d30c2a0746c3ea8091d1a28cbf3b066165b883a6
markshawn2020 <shawninjuly@gmail.com>
2024-01-27 09:35:20 +0800

    init with shadcn

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "next/core-web-vitals",
+  ],
+  rules: {
+  },
+};
+
+module.exports = config;
