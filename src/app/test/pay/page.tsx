da40031e27472f8d9438540d8b28467a3b51a97c
markshawn2020 <shawninjuly@gmail.com>
2024-02-14 03:29:44 +0800

    :sparkles: good pay page

--- src/app/test/pay/page.tsx
+++ src/app/test/pay/page.tsx
@@ -2,62 +2,16 @@
 
 import { Button } from "@/components/ui/button"
 import { VerticalContainer } from "@/components/containers/vertical"
-import { useCopyToClipboard } from "@uidotdev/usehooks"
-import QRCode from "qrcode.react"
-import { useEffect, useState } from "react"
 import { nanoid } from "nanoid"
-import {
-  PaymentOtherStatus,
-  PaymentStatus,
-  PayQueryResData,
-} from "@/lib/pay/schema"
 import { useRunningEnvironment } from "@/hooks/use-running-environment"
 import { No, Yes } from "@/components/_universal/icons"
-import { Label } from "@/components/ui/label"
-import { toast } from "sonner"
-import { cancelJob, createPrepayAction } from "@/lib/pay/actions"
-import { initPusherClient } from "@/lib/socket/config"
-import { SocketEventType } from "@/lib/socket/events"
-import { api } from "@/lib/trpc/react"
-import { PayStep } from "@/components/pay"
+import { createPrepayAction } from "@/lib/pay/actions"
+import { JumpPay } from "@/components/pay"
 
 export default function TestPayPage() {
-  const [payUrl, copyInvoiceUrl] = useCopyToClipboard()
-  const [payStatus, setPayStatus] = useState<PaymentStatus | null>(null)
-  const [payId, setPayId] = useState("")
-
   const { isWechat, isMobile } = useRunningEnvironment()
   const IsWechat = isWechat ? Yes : No
   const IsMobile = isMobile ? Yes : No
-
-  const clean = () => {
-    cancelJob(payId)
-  }
-
-  useEffect(() => {
-    if (!payId) return
-
-    window.addEventListener("beforeunload", clean)
-    const pusher = initPusherClient()
-    const channel = pusher.subscribe(payId)
-    channel.bind(SocketEventType.Payment, (data: PayQueryResData) => {
-      console.log("-- received data: ", data)
-      setPayStatus(data.order_status)
-    })
-
-    return () => {
-      clean()
-      window.removeEventListener("beforeunload", clean)
-      channel.unbind(SocketEventType.Payment)
-      pusher.unsubscribe(payId)
-    }
-  }, [payId])
-
-  const charge = api.bill.charge.useMutation()
-
-  useEffect(() => {}, [payId])
-
-  // const getWxAccount = api.user.wxAuth.useMutation()
 
   return (
     <VerticalContainer>
@@ -65,31 +19,7 @@ export default function TestPayPage() {
         微信 <IsWechat /> 手机 <IsMobile />
       </div>
 
-      <Button
-        onClick={async () => {
-          setPayStatus(null)
-
-          // clean before
-          console.log("-- clicked")
-          if (payId) await cancelJob(payId)
-
-          console.log("-- creating")
-          const { url: invoiceUrl, id } = await charge.mutateAsync({
-            value: 1,
-          })
-          setPayId(id)
-          console.log("-- res: ", invoiceUrl)
-          await copyInvoiceUrl(invoiceUrl)
-          // window.location.href = `weixin://dl/` + invoiceUrl
-
-          // 手机微信浏览器直接跳转
-          if (isWechat && isMobile) location.href = invoiceUrl
-        }}
-      >
-        跳转支付
-      </Button>
-
-      {payUrl && <ShowInvoice invoice={payUrl} payStatus={payStatus} />}
+      <JumpPay />
 
       <Button
         onClick={async () => {
@@ -104,39 +34,5 @@ export default function TestPayPage() {
         预下单支付
       </Button>
     </VerticalContainer>
-  )
-}
-
-const ShowInvoice = ({
-  invoice,
-  payStatus,
-}: {
-  invoice: string
-  payStatus: null | PaymentStatus
-}) => {
-  const { isWechat, isMobile } = useRunningEnvironment()
-  const [invoiceUrl, copyInvoiceUrl] = useCopyToClipboard()
-
-  if (isMobile && isWechat) return
-
-  return (
-    <div className={"p-4 flex flex-col items-start gap-4"}>
-      <PayStep status={payStatus} />
-
-      <Label>方法一：（微信/支付宝）扫码支付</Label>
-      <QRCode value={invoice} />
-
-      <div>方法二：复制链接到手机微信中打开</div>
-      <Button
-        onClick={() => {
-          copyInvoiceUrl(invoice)
-          toast.success(`复制链接成功：${invoice}`)
-        }}
-      >
-        复制
-      </Button>
-
-      {/*<Button onClick={() => cancelJob(id)}>取消</Button>*/}
-    </div>
   )
 }
--- src/components/pay.tsx
+++ src/components/pay.tsx
@@ -1,9 +1,135 @@
-import { PaymentStatus } from "@/lib/pay/schema"
+import { PaymentStatus, PayQueryResData } from "@/lib/pay/schema"
 import { cn } from "@/lib/utils"
+import { useRunningEnvironment } from "@/hooks/use-running-environment"
+import { Separator } from "@/components/ui/separator"
+import { Button } from "@/components/ui/button"
+import { useCopyToClipboard } from "@uidotdev/usehooks"
+import { useEffect, useState } from "react"
+import { initPusherClient } from "@/lib/socket/config"
+import { SocketEventType } from "@/lib/socket/events"
+import { api } from "@/lib/trpc/react"
+import { cancelJob } from "@/lib/pay/actions"
+import { Label } from "@/components/ui/label"
+import { ThumbsUpIcon } from "lucide-react"
+import QRCode from "qrcode.react"
+import { toast } from "sonner"
+
+export const JumpPay = () => {
+  const [payUrl, copyInvoiceUrl] = useCopyToClipboard()
+  const [payStatus, setPayStatus] = useState<PaymentStatus | null>(null)
+  const [payId, setPayId] = useState("")
+
+  const { isWechat, isMobile } = useRunningEnvironment()
+
+  useEffect(() => {
+    if (!payId) return
+
+    const pusher = initPusherClient()
+    const channel = pusher.subscribe(payId)
+    channel.bind(SocketEventType.Payment, (data: PayQueryResData) => {
+      console.log("-- received data: ", data)
+      setPayStatus(data.order_status)
+    })
+
+    return () => {
+      channel.unbind(SocketEventType.Payment)
+      pusher.unsubscribe(payId)
+    }
+  }, [payId])
+
+  const charge = api.bill.charge.useMutation()
+
+  const retry = async () => {
+    setPayStatus(null)
+
+    // clean before
+    console.log("-- clicked")
+    if (payId) await cancelJob(payId)
+
+    console.log("-- creating")
+    const { url: invoiceUrl, id } = await charge.mutateAsync({
+      value: 1,
+    })
+    setPayId(id)
+    console.log("-- res: ", invoiceUrl)
+    await copyInvoiceUrl(invoiceUrl)
+    // window.location.href = `weixin://dl/` + invoiceUrl
+
+    // 手机微信浏览器直接跳转
+    if (isWechat && isMobile) location.href = invoiceUrl
+  }
+
+  return (
+    <div className={"flex flex-col items-center"}>
+      <Button onClick={retry}>跳转支付</Button>
+
+      {payUrl && (
+        <ShowPayComp retry={retry} url={payUrl} payStatus={payStatus} />
+      )}
+    </div>
+  )
+}
+
+export const ShowPayComp = ({
+  url,
+  payStatus,
+  retry,
+}: {
+  url: string
+  payStatus: null | PaymentStatus
+  retry: () => void
+}) => {
+  const { isWechat, isMobile } = useRunningEnvironment()
+
+  if (isMobile && isWechat) return
+
+  return (
+    <div className={"p-4 flex flex-col items-center gap-6"}>
+      <PayStep status={payStatus} />
+
+      <Separator orientation={"horizontal"} />
+
+      {payStatus !== "TIMEOUT" ? (
+        <ShowPaying url={url} />
+      ) : (
+        <Button className={"w-full"} onClick={retry}>
+          重试
+        </Button>
+      )}
+    </div>
+  )
+}
+
+export const ShowPaying = ({ url }: { url: string }) => {
+  const [copiedUrl, copyUrl] = useCopyToClipboard()
+
+  return (
+    <>
+      <Label className={"inline-flex items-center gap-1"}>
+        <ThumbsUpIcon className={"w-4 h-4 text-primary"} />
+        方法一：（微信/支付宝）扫码支付
+      </Label>
+      <QRCode value={url} size={192} className={"mx-auto"} />
+
+      <Separator orientation={"horizontal"} />
+
+      <div>方法二：复制链接到手机微信中打开</div>
+      <Button
+        className={"w-full"}
+        onClick={() => {
+          copyUrl(url)
+          toast.success(`复制链接成功：${url}`)
+        }}
+      >
+        复制
+      </Button>
+    </>
+  )
+}
 
 export const PayStep = ({ status }: { status: PaymentStatus | null }) => {
   return (
-    <ul className="steps steps-vertical w-full">
+    <ul className="steps w-full ">
       {/*<Label className={"text-green-500"}>*/}
       {/*  <span> 状态：</span>*/}
       {/*  <span>{invoiceStatus}</span>*/}
