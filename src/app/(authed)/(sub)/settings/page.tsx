6a8f2fd9419441d61104d834834bc806f471f03c
markshawn2020 <shawninjuly@gmail.com>
2024-02-19 10:21:46 +0800

    settings add wxid

---
+++ src/app/(authed)/(sub)/settings/page.tsx
@@ -0,0 +1,61 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { maskPhone } from "@/lib/utils"
+import { useUser } from "@/hooks/use-user"
+import { TODO } from "@/config"
+import { signOut } from "next-auth/react"
+import { Label } from "@/components/ui/label"
+import { BasicMutableUserInfo } from "@/components/user/basic"
+import { GrayCard } from "@/components/_universal/cards"
+import { MenuItem } from "@/components/_universal/line"
+import SubPage from "@/components/sub-page"
+import { toast } from "sonner"
+
+export default function SettingsPage() {
+  const user = useUser()
+
+  const phone = user?.phone
+  const wxid = user?.wxid
+
+  return (
+    <SubPage title={"用户中心"} className={"flex flex-col gap-4"}>
+      <BasicMutableUserInfo />
+
+      <GrayCard>
+        <MenuItem name={"微信登录"}>
+          {wxid ?? (
+            <Button
+              variant={"ghost"}
+              onClick={() => {
+                toast.info(TODO)
+              }}
+            >
+              点击绑定
+            </Button>
+          )}
+        </MenuItem>
+        <MenuItem name={"手机绑定"}>
+          {phone ? (
+            maskPhone(phone)
+          ) : (
+            <Button
+              variant={"ghost"}
+              onClick={() => {
+                toast.info(TODO)
+              }}
+            >
+              点击绑定
+            </Button>
+          )}
+        </MenuItem>
+      </GrayCard>
+
+      <GrayCard>
+        <MenuItem name={"私域管理中心"}>{TODO}</MenuItem>
+      </GrayCard>
+
+      <Button onClick={() => signOut()}>退出登录</Button>
+    </SubPage>
+  )
+}
